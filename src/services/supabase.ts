8b59e3402638e8abf6b6108222d8057ab1b135dc
unknown <alexeyzharsky@gmail.com>
2026-08-20 13:26:39 +0300

    supabase db types added

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types/database.types";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
@@ -11,4 +12,7 @@ if (!supabasePublishableKey) {
   throw new Error("Missing VITE_SUPABASE_PUBLISHABLE_KEY environment variable");
 }
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey);
+export const supabase = createClient<Database>(
+  supabaseUrl,
+  supabasePublishableKey,
+);
